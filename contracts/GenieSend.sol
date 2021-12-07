42c7c0408b39661b601019ffae0d5a50aa0fdc94
Jason Cai <jason.y.cai@gmail.com>
2021-12-06 21:14:30 -0500

    Initial commit

---
+++ contracts/GenieSend.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+contract GenieSend {
+
+    struct Erc721Transfer {
+        address erc721;
+        address[] recipients;
+        uint[] tokenIds;
+    }
+
+    struct Erc1155Transfer {
+        address erc1155;
+        address[] recipients;
+        uint[] tokenIds;
+        uint[] quantities;
+    }
+
+    constructor() {}
+
+    function multisend(
+        Erc721Transfer[] calldata erc721transfers,
+        Erc1155Transfer[] calldata erc1155transfers
+    ) public {
+        for (uint i=0; i < erc721transfers.length; i++) {
+            require(
+                erc721transfers[i].recipients.length == erc721transfers[i].tokenIds.length,
+                "Mismatched recipients and tokenIds length in ERC 721"
+            );
+            for (uint j=0; j < erc721transfers[i].recipients.length; j++) {
+                IERC721(erc721transfers[i].erc721).safeTransferFrom(
+                    msg.sender,
+                    erc721transfers[i].recipients[j],
+                    erc721transfers[i].tokenIds[j]
+                );
+            }
+        }
+        for (uint i=0; i < erc1155transfers.length; i++) {
+            require(
+                erc1155transfers[i].recipients.length == erc1155transfers[i].tokenIds.length,
+                "Mismatched recipients and tokenIds length in ERC 1155"
+            );
+            ERC1155Transfer transfer = erc1155transfers[i];
+            for (uint j=0; j < transfer.recipients.length; j++) {
+                IERC1155(transfer.erc1155).safeTransferFrom(
+                    msg.sender,
+                    trasnfer.recipients[j]
+                    transfer.tokenIds[j],
+                    transfer.quantities[j]
+                );
+            }
+        }
+    }
+}
